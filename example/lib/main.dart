60b2d29cdbf08965bb39e4f1339125469f22dd8b
Xoldarov Temur <temuruz2003@gmail.com>
2024-03-02 22:24:38 +0500

    add example

---
+++ example/lib/main.dart
@@ -0,0 +1,82 @@
+import 'package:dio/dio.dart';
+import 'package:flutter/material.dart';
+import 'package:tx_dio_interceptor/tx_dio_interceptor.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  final dio = Dio();
+
+  bool _refreshToken() {
+    return true;
+  }
+
+  void _request() {
+    final refreshTokenInterceptor = TXRefreshTokenInterceptor(
+        dio: dio,
+        onRefreshToken: () async {
+          return _refreshToken();
+        },
+        shouldRefreshToken: (response) {
+          return response.statusCode == 401;
+        },
+        onRefreshTokenFailed: (response) async {
+          //refresh token failed
+        });
+    dio.interceptors.add(refreshTokenInterceptor);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              'Tap Button to test',
+              style: Theme.of(context).textTheme.headlineMedium,
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _request,
+        tooltip: 'Test button',
+        child: const Icon(Icons.add),
+      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
